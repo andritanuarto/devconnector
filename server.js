747026ae453a8593fe7030cc9df03fdc886fb182
Andri Tanuarto <andritanuarto@gmail.com>
2019-05-16 12:50:39 -0700

    User request and validation

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+//Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => { res.send('API Running'); });
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Connect Database
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
