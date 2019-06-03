c9c39bcf5cb0267cbca4d5b817b3d210a0588037
Andri Tanuarto <andritanuarto@gmail.com>
2019-06-02 17:03:40 -0700

    Update git ignore and setting up reducer alert state

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,24 @@ import Landing from './components/layout/landing';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={ Landing } />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/login" component={ Login } />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={ Landing } />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
