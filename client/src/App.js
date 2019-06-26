24ad78f8f29ea996f7f762b0205e44327c32b0d7
Andri Tanuarto <andritanuarto@gmail.com>
2019-06-26 14:22:00 -0700

    Add protected routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import Landing from './components/layout/landing';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import Alert from './components/layout/alert';
-
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+//Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
             </Switch>
           </section>
         </Fragment>
